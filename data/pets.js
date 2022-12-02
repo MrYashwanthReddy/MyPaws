5093dcc59fda5d04a68e3a87616d9fe337278ff6
Yashwanth Reddy V <yashwanthreddyveesarapu@gmail.com>
2022-12-02 00:26:35 -0500

    Yashwanth Reddy - PETS JS - 12/01/2022

--- data/pets.js
+++ data/pets.js
@@ -6,11 +6,24 @@ const foundpets = mongoCollections.foundpets;
 const getLostpets = async () => {
   try {
     const lostPetsCollection = await lostpets();
+
+    const lostpetData = await lostPetsCollection.find({}).toArray();
+
+    return lostpetData;
   } catch (error) {}
 };
 
 const getFoundpets = async () => {
   try {
     const foundPetsCollection = await foundpets();
+
+    const foundpetData = await foundPetsCollection.find({}).toArray();
+
+    return foundpetData;
   } catch (error) {}
+};
+
+module.exports = {
+  getLostpets,
+  getFoundpets,
 };
