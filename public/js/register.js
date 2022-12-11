a6a29c724cb1a34ea5cfd3be3ab774183d3c38cc
Yashwanth Reddy Veesarapu <116411371+MrYashwanthReddy@users.noreply.github.com>
2022-12-11 08:44:57 -0500

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -1,6 +1,7 @@
 let outputProfileImage = document.getElementById("outputProfileImage");
 let inputProfileImage = document.getElementById("profileImageInput");
 
+outputProfileImage.style.display = "none";
 
 inputProfileImage.addEventListener("change", (e) => {
   outputProfileImage.style.display = "flex";
