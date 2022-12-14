c0b0bb57c455f3ebfa05a97c15f64355e17820f4
Yashwanth Reddy Veesarapu <yashwanthreddyveesarapu@gmail.com>
2022-12-14 03:31:48 -0500

    12/14/2022 - Minor

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,6 @@ router
     let error = xss(req.query.e);
     if (error == "l") {
       error = "Error: Login Required to Proceed";
-      res.render("home/landing", { page: { title: "Home" } });
     }
     res.render("users/login", { page: { title: "Login" }, error: error });
   })
