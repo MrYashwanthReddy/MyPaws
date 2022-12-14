f27d1f3c2c8360bb202ddbd990d85e34b98223c0
byte-archive <prathamk.eng@gmail.com>
2022-12-14 13:44:45 -0500

    Update lost and found page

--- data/users.js
+++ data/users.js
@@ -1,17 +1,17 @@
 const mongoCollections = require("../config/mongoCollections");
-const { validValue, checkString, checkId } = require("../validation");
+const {validValue, checkString, checkId} = require("../validation");
 
 const users = mongoCollections.users;
 
 const bcrypt = require("bcrypt");
-const { ObjectId } = require("mongodb");
+const {ObjectId} = require("mongodb");
 
 const getAllUsers = async () => {
   let usersCollection;
   try {
     usersCollection = await users();
   } catch (error) {
-    throw { status: 500, msg: "Error: Server error" };
+    throw {status: 500, msg: "Error: Server error"};
   }
 
   const data = await usersCollection.find({}).toArray();
@@ -29,38 +29,37 @@ const createUser = async ({
   password,
   profileImage,
 }) => {
-  let usersCollection;
   try {
-    usersCollection = await users();
+    const usersCollection = await users();
+
+    const user = await usersCollection.findOne({email: email});
+
+    if (user) {
+      throw {status: 403, msg: "User already exists"};
+    }
+
+    const userData = {
+      firstName,
+      lastName,
+      age,
+      email,
+      petName,
+      petBreed,
+      password,
+      profileImage,
+    };
+
+    const insertInfo = await usersCollection.insertOne(userData);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId)
+      throw {status: 400, msg: "Could not add user"};
+
+    const newId = insertInfo.insertedId.toString();
+    const newUser = await getUserById(newId);
+
+    return {status: 200, insertedUser: true, data: newUser};
   } catch (error) {
-    throw { status: 500, msg: "Error: Server error" };
+    throw {status: 500, msg: "Error: Server error"};
   }
-
-  const user = await usersCollection.findOne({ email: email });
-
-  if (user) {
-    throw { status: 403, msg: "User already exists" };
-  }
-
-  const userData = {
-    firstName,
-    lastName,
-    age,
-    email,
-    petName,
-    petBreed,
-    password,
-    profileImage,
-  };
-
-  const insertInfo = await usersCollection.insertOne(userData);
-  if (!insertInfo.acknowledged || !insertInfo.insertedId)
-    throw { status: 400, msg: "Could not add user" };
-
-  const newId = insertInfo.insertedId.toString();
-  const newUser = await getUserById(newId);
-
-  return { status: 200, insertedUser: true, data: newUser };
 };
 
 const getUserById = async (id) => {
@@ -71,12 +70,12 @@ const getUserById = async (id) => {
   try {
     usersCollection = await users();
   } catch (error) {
-    throw { status: 500, msg: "Error: Server error" };
+    throw {status: 500, msg: "Error: Server error"};
   }
 
-  const user = await usersCollection.findOne({ _id: ObjectId(id) });
+  const user = await usersCollection.findOne({_id: ObjectId(id)});
 
-  if (user === null) throw { status: 404, msg: "Error: No User with that id" };
+  if (user === null) throw {status: 404, msg: "Error: No User with that id"};
 
   return user;
 };
@@ -86,19 +85,19 @@ const userLogin = async (email, pass) => {
   try {
     usersCollection = await users();
   } catch (error) {
-    throw { status: 500, msg: "Error: Server error" };
+    throw {status: 500, msg: "Error: Server error"};
   }
 
-  const user = await usersCollection.findOne({ email: email });
+  const user = await usersCollection.findOne({email: email});
   if (user == null) {
-    throw { status: 401, msg: "Invalid Username or Password" };
+    throw {status: 401, msg: "Invalid Username or Password"};
   }
   let dbPass = validValue(user.password);
 
   const validPassword = await bcrypt.compare(pass, dbPass);
 
-  if (validPassword) return { status: 200, ...user };
-  else throw { status: 401, msg: "Invalid Username or Password" };
+  if (validPassword) return {status: 200, ...user};
+  else throw {status: 401, msg: "Invalid Username or Password"};
 };
 
 module.exports = {
